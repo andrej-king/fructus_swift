0254b30f3d26258596410d3b0a63b55d5936d789
Andrej Pedchenko <graduadu@gmail.com>
2022-03-14 19:36:30 +0200

    Fixed fruit model key. Added fruit data

--- Fructus/Model/FruitModel.swift
+++ Fructus/Model/FruitModel.swift
@@ -5,7 +5,7 @@ import SwiftUI
 struct Fruit: Identifiable {
     var id = UUID()
     var title: String
-    var hadline: String
+    var headline: String
     var image: String
     var gradientColors: [Color]
     var description: String
