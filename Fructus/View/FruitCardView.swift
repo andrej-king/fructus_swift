ec208a1c382bd453528a17763652e2fbbe502ad0
Andrej Pedchenko <graduadu@gmail.com>
2022-03-14 17:45:21 +0200

    Added blueberry img

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -7,6 +7,11 @@ struct FruitCardView: View {
     var body: some View {
         VStack(spacing: 20) {
             // FRUIT: IMAGE
+            Image("blueberry")
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+            
             // FRUIT: TITLE
             Text("Blueberry")
             // FRUIT: HEADLINE
