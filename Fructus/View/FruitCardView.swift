ed662988bc232b8b6711c20999a9b81e7b8cf53e
Andrej Pedchenko <graduadu@gmail.com>
2022-03-14 18:15:45 +0200

    Added title

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -15,6 +15,11 @@ struct FruitCardView: View {
                 
                 // FRUIT: TITLE
                 Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
                 // FRUIT: HEADLINE
                 // BUTTON: START
             } //: VSTACK
