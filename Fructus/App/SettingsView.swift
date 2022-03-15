d3772792c02c9e74bdfa5f8402159d17d8a6419b
Andrej Pedchenko <graduadu@gmail.com>
2022-03-15 22:36:27 +0200

    Added GroupBox to the SettingsView. Created SettingsLabelView.

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -11,7 +11,32 @@ struct SettingsView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    Text("Hello world")
+                    // MARK: - SECTION 1
+                    
+                    GroupBox(
+                        label:
+                            SettingsLabelView(
+                                labelText: "Fructus",
+                                labelImage: "info.circle"
+                            )
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10) {
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        } //: HSTACK
+                    }
+                    
+                    // MARK: - SECTION 2
+                    
+                    // MARK: - SECTION 3
+                    
                 } //: VSCTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
                 .navigationBarItems(
