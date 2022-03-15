40f9e7515cd691ba2ecdf5e725d4495c4031742e
Andrej Pedchenko <graduadu@gmail.com>
2022-03-15 20:41:10 +0200

    Added FruitNutrientsView

---
+++ Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODy
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView()
+    }
+}
